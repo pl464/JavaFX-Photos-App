fe73dac99db05096d0ecae6786e9861b72241863
Lance Luo <lance.luo@rutgers.edu>
2020-04-08 15:51:17 +0000

    added search logic

--- Photos23/src/photos/view/SearchController.java
+++ Photos23/src/photos/view/SearchController.java
@@ -2,4 +2,47 @@ package photos.view;
 
 public class SearchController extends Controller{
 
+	public ArrayList<String> searchByDate(LocalDateTime start, LocalDateTime end) {
+		
+		ArrayList<String> paths = new ArrayList<String>();
+		for (Map.Entry<String, Image> e : currUser.images.entrySet()) {
+			if (e.getValue().date.compareTo(start) >= 0 && e.getValue().date.compareTo(end) <= 0) {
+				paths.add(e.getKey());
+			}
+		}
+		return paths;
+	}
+
+	public ArrayList<String> searchByTag(String tag, String val) {
+		
+		ArrayList<String> paths = new ArrayList<String>();
+		for (Map.Entry<String, Image> e : currUser.images.entrySet()) {
+			if (e.getValue().tags.get(tag).contains(val)) {
+				paths.add(e.getKey());
+			}
+		}
+		return paths;
+	}
+	
+	public ArrayList<String> searchByAnd(String tag1, String val1, String tag2, String val2) {
+		
+		ArrayList<String> paths = new ArrayList<String>();
+		for (Map.Entry<String, Image> e : currUser.images.entrySet()) {
+			if (e.getValue().tags.get(tag1).contains(val1) && e.getValue().tags.get(tag2).contains(val2)) {
+				paths.add(e.getKey());
+			}
+		}
+		return paths;
+	}
+	
+	public ArrayList<String> searchByOr(String tag1, String val1, String tag2, String val2) {
+		
+		ArrayList<String> paths = new ArrayList<String>();
+		for (Map.Entry<String, Image> e : currUser.images.entrySet()) {
+			if (e.getValue().tags.get(tag1).contains(val1) || e.getValue().tags.get(tag2).contains(val2)) {
+				paths.add(e.getKey());
+			}
+		}
+		return paths;
+	}
 }
