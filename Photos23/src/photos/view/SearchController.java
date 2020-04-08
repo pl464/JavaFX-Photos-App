5f30d4805f7ec0084acd22a5f223cfb65b934fd0
Lance Luo <lance.luo@rutgers.edu>
2020-04-08 16:13:34 +0000

    added searching within an album

--- Photos23/src/photos/view/SearchController.java
+++ Photos23/src/photos/view/SearchController.java
@@ -1,11 +1,20 @@
 package photos.view;
 
-public class SearchController extends Controller{
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Map;
 
-	public ArrayList<String> searchByDate(LocalDateTime start, LocalDateTime end) {
+import photos.Image;
+
+public class SearchController extends Controller{
+	
+	public ArrayList<String> searchByDate(LocalDateTime start, LocalDateTime end, boolean single) {
 		
 		ArrayList<String> paths = new ArrayList<String>();
 		for (Map.Entry<String, Image> e : currUser.images.entrySet()) {
+			if (single && !currUser.albums.get(currAlbum).contains(e.getKey())) {
+				continue;
+			}
 			if (e.getValue().date.compareTo(start) >= 0 && e.getValue().date.compareTo(end) <= 0) {
 				paths.add(e.getKey());
 			}
@@ -13,10 +22,13 @@ public class SearchController extends Controller{
 		return paths;
 	}
 
-	public ArrayList<String> searchByTag(String tag, String val) {
+	public ArrayList<String> searchByTag(String tag, String val, boolean single) {
 		
 		ArrayList<String> paths = new ArrayList<String>();
 		for (Map.Entry<String, Image> e : currUser.images.entrySet()) {
+			if (single && !currUser.albums.get(currAlbum).contains(e.getKey())) {
+				continue;
+			}
 			if (e.getValue().tags.get(tag).contains(val)) {
 				paths.add(e.getKey());
 			}
@@ -24,10 +36,13 @@ public class SearchController extends Controller{
 		return paths;
 	}
 	
-	public ArrayList<String> searchByAnd(String tag1, String val1, String tag2, String val2) {
+	public ArrayList<String> searchByAnd(String tag1, String val1, String tag2, String val2, boolean single) {
 		
 		ArrayList<String> paths = new ArrayList<String>();
 		for (Map.Entry<String, Image> e : currUser.images.entrySet()) {
+			if (single && !currUser.albums.get(currAlbum).contains(e.getKey())) {
+				continue;
+			}
 			if (e.getValue().tags.get(tag1).contains(val1) && e.getValue().tags.get(tag2).contains(val2)) {
 				paths.add(e.getKey());
 			}
@@ -35,10 +50,13 @@ public class SearchController extends Controller{
 		return paths;
 	}
 	
-	public ArrayList<String> searchByOr(String tag1, String val1, String tag2, String val2) {
+	public ArrayList<String> searchByOr(String tag1, String val1, String tag2, String val2, boolean single) {
 		
 		ArrayList<String> paths = new ArrayList<String>();
 		for (Map.Entry<String, Image> e : currUser.images.entrySet()) {
+			if (single && !currUser.albums.get(currAlbum).contains(e.getKey())) {
+				continue;
+			}
 			if (e.getValue().tags.get(tag1).contains(val1) || e.getValue().tags.get(tag2).contains(val2)) {
 				paths.add(e.getKey());
 			}
