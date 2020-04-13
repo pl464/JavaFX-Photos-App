8b37d6ee10edd689586786454553e706ba7bae79
Patrick <pl464@scarletmail.rutgers.edu>
2020-04-13 13:03:25 -0400

    Centered images in photo display

--- Photos23/src/photos/view/PhotoDisplayController.java
+++ Photos23/src/photos/view/PhotoDisplayController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -17,10 +18,12 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
 import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import photos.Picture;
 
