4456822e86724f1a04d2f7d57021d1b173548704
unknown <lance.luo@rutgers.edu>
2020-04-14 21:26:36 -0400

    stock photos fix

--- Photos23/src/photos/Photos.java
+++ Photos23/src/photos/Photos.java
@@ -1,4 +1,6 @@
 package photos;
+import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import javafx.application.Application;
@@ -41,6 +43,34 @@ public class Photos extends Application {
 	public void start(Stage stage) throws Exception {
 		sceneManager = new SceneManager(stage);
 		users = sceneManager.readUsers();
+		
+		if (users.containsKey("stock") == false) {
+			users.put("stock", new User());
+			users.get("stock").albums.put("stock", new ArrayList<String>());
+			String currDir = System.getProperty("user.dir");
+			String a = currDir + File.separator + "data" + File.separator + "blathers.jpg";
+			String b = currDir + File.separator + "data" + File.separator + "isabelle.jpg";
+			String c = currDir + File.separator + "data" + File.separator + "kkslider.jpg";
+			String d = currDir + File.separator + "data" + File.separator + "mrresetti.jpg";
+			String e = currDir + File.separator + "data" + File.separator + "tomnook.jpg";
+			File a1 = new File(a);
+			File b1 = new File(b);
+			File c1 = new File(c);
+			File d1 = new File(d);
+			File e1 = new File(e);
+			users.get("stock").albums.get("stock").add(a1.toURI().toString());
+			users.get("stock").albums.get("stock").add(b1.toURI().toString());
+			users.get("stock").albums.get("stock").add(c1.toURI().toString());
+			users.get("stock").albums.get("stock").add(d1.toURI().toString());
+			users.get("stock").albums.get("stock").add(e1.toURI().toString());
+			users.get("stock").pictures.put(a1.toURI().toString(), new Picture(a1));
+			users.get("stock").pictures.put(b1.toURI().toString(), new Picture(b1));
+			users.get("stock").pictures.put(c1.toURI().toString(), new Picture(c1));
+			users.get("stock").pictures.put(d1.toURI().toString(), new Picture(d1));
+			users.get("stock").pictures.put(e1.toURI().toString(), new Picture(e1));
+			sceneManager.writeUsers(users);
+		}
+		
 		this.mainStage = stage;
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(getClass().getResource("view/Login_Window.fxml"));
